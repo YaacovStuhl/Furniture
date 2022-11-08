c9d9b50540b3ea1812685d561c040778fc42199d
lbenas <117137037+lbenas@users.noreply.github.com>
2022-11-07 22:58:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,47 @@ function removeActiveClasses(){
         panel.classList.remove('active')
     })
 }
+
+// wave js
+
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
+
+// search js
+
+const search = document.querySelector('.search');
+const btn = document.querySelector('.bton');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () =>{
+    search.classList.toggle('active');
+    input.focus();
+})
+
+// scroll js
+
+const boxes = document.querySelectorAll('.slide')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+   const triggerBottom = window.innerHeight / 5 * 4
+
+   boxes.forEach(slide => {
+      const boxTop = slide.getBoundingClientRect().top
+
+      if(boxTop < triggerBottom) {
+         slide.classList.add('show')
+      } else {
+         slide.classList.remove('show')
+      }
+   })
+}
